0a1b1facb1795d9cb569598a50006a23ce7bbfec
khaledibrahim7 <khaledramadanza@gmail.com>
2026-02-04 17:51:18 +0200

    Fix docker image and paths

---
+++ src/main/java/com/giudeMuslim/mauqetAlsallah/controller/AzkarController.java
@@ -0,0 +1,25 @@
+package com.giudeMuslim.mauqetAlsallah.controller;
+
+import com.giudeMuslim.mauqetAlsallah.service.AzkarService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/azkar")
+@RestController
+@RequiredArgsConstructor
+public class AzkarController {
+    private final AzkarService AzkarService;
+
+
+    @GetMapping("/salyAlaELnaby")
+    public String salyAlaELnaby() {
+        return "صلي ع النبي";
+    }
+
+    @GetMapping
+    public String getRandomZikr() {
+        return AzkarService.RandomZikr();
+    }
+}
